64a1756193d7f0fcd265019e7416b284434aa1d2
XtremeTHN <xtreme.dev2@gmail.com>
2025-01-05 23:47:30 -0600

    implemented upload alert to the code

--- src/ui/dialogs.py
+++ src/ui/dialogs.py
@@ -10,4 +10,12 @@ class GetStartedDialog(Adw.Dialog):
     def get_started_clicked(self, _):
         self.close()
 
-# @Gtk.Template(resource_path="/com/github/XtremeTHN/NXLoader/upload-confirmation.ui")
+@Gtk.Template(resource_path="/com/github/XtremeTHN/NXLoader/upload-alert.ui")
+class UploadAlert(Adw.AlertDialog):
+    __gtype_name__ = "UploadAlert"
+    def __init__(self, settings):
+        super().__init__()
+        self.settings = settings
+
+    def do_not_show_again(self, check: Gtk.CheckButton):
+        self.settings.set_boolean("show-upload-alert", check.get_active() is True)
