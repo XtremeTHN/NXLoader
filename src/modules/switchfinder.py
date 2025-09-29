be0d0c67b9476921a8c73a86d729d348809d9280
Axel <nigthmaresans2@gmail.com>
2025-09-28 18:15:02 -0600

    added udev rule fixed broken icon on rom page

--- src/modules/switchfinder.py
+++ src/modules/switchfinder.py
@@ -2,40 +2,43 @@ from gi.repository import GObject, GUdev
 from .usbInstall import SwitchUsb
 from usb.core import USBError
 
+
 class SwitchFinder(GObject.GObject):
     __gsignals__ = {
         "disconnected": (GObject.SIGNAL_RUN_FIRST, None, tuple()),
-        "connected": (GObject.SIGNAL_RUN_FIRST, None, (str,))
+        "connected": (GObject.SIGNAL_RUN_FIRST, None, (str,)),
     }
+
     def __init__(self):
         super().__init__()
 
         self.protocol = SwitchUsb()
-    
+
     def set_switch(self, dev):
         msg = ""
         try:
             self.protocol.set_switch(dev)
-        except USBError:
+        except USBError as e:
+            print(e.args)
             msg = "Permission denied"
         finally:
             self.emit("connected", msg)
-    
+
     def start(self):
         # if the switch is connected before GUdev.Client initialize, then emit the connected signal
-        if (n:=self.protocol.find_switch()) is not None:
+        if (n := self.protocol.find_switch()) is not None:
             self.set_switch(n)
 
         self.client = GUdev.Client.new(["usb/usb_interface"])
-        self.client.connect('uevent', self.__create_obj)
+        self.client.connect("uevent", self.__create_obj)
 
     def __create_obj(self, _, action: str, device: GUdev.Device):
         # Check if this device is a nintendo switch
         if device.get_property("ID_VENDOR_FROM_DATABASE") != "Nintendo Co., Ltd":
             return
-        
+
         # PRODUCT is something like this: 57e/3000/100
-        if (p:=device.get_property("PRODUCT")) is not None:
+        if (p := device.get_property("PRODUCT")) is not None:
             p = p.split("/")
             if len(p) < 2:
                 return
